366cecfe731bb2dde17cee3c689b30176e7439b5
christophcannz <christophcannz@gmail.com>
2019-05-30 17:17:29 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Olivia Munn",
+        photos: "image.png",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Diana Agron",
+        photos: "image.png",
+        scores: [2, 5, 8, 1, 9, 4, 8, 3, 7, 5]
+    },
+    {
+        name: "Aubrey Plaza",
+        photos: "image.png",
+        scores: [5, 7, 8, 9, 4, 3, 5, 8, 1, 7]
+    },
+    {
+        name: "Norm MacDonald",
+        photos: "image.png",
+        scores: [6, 3, 2, 6, 9, 6, 4, 2, 1, 5]
+    },
+    {
+        name: "Gilbert Gottfried",
+        photos: "image.png",
+        scores: [4, 2, 6, 3, 7, 9, 8, 2, 1, 7]
+    },
+    {
+        name: "Carrot Top",
+        photos: "image.png",
+        scores: [1, 6, 5, 8, 2, 7, 4, 9, 5, 3]
+    },
+];
+
+module.exports = friends;
